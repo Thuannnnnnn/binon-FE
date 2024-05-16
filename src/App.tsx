d7c0ef1afb95d6489a4c9f4e90e8ad24301165de
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-05-16 20:58:31 +0700

    a

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
 
   return (
     <>
-      <h1>test 3</h1>
+      <h1>{userData}</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -41,8 +41,7 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <h2>User Data</h2>
-        <p>{userData}</p>
+
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
